e7935a719da9bf173b7fe252093383fea9f6f571
Toureiro2 <joao_toureiro@hotmail.com>
2023-12-13 19:30:56 -0300

    Criadas as classes UsuarioDAO e UsuarioDAOImpl

---
+++ src/main/java/br/senac/modelo/dao/usuario/UsuarioDAO.java
@@ -0,0 +1,16 @@
+package br.senac.modelo.dao.usuario;
+
+import java.util.List;
+
+import br.senac.modelo.entidade.usuario.Usuario;
+
+public interface UsuarioDAO {
+
+    void inserirUsuario(Usuario usuario);
+
+    void deletarUsuario(Usuario usuario);
+
+    void atualizarUsuario(Usuario usuario);
+
+    List<Usuario> recuperarUsuario();
+}
