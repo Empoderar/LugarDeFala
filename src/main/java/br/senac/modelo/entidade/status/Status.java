96eec7ff20a25aec049c93502a364c8ae634ea1e
Brenda <eisingbrenda@gmail.com>
2023-12-13 17:38:37 -0300

    Feito o mapeamento da classe Status

--- src/main/java/br/senac/modelo/entidade/status/Status.java
+++ src/main/java/br/senac/modelo/entidade/status/Status.java
@@ -1,5 +1,31 @@
 package br.senac.modelo.entidade.status;
 
-public enum Status {
-	APROVADO, REJEITADO, PENDENTE, ARQUIVADO
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "status")
+public class Status implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_status")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private StatusEnum statusEnum;
+
+    public Status() {}
+
+    public enum StatusEnum {
+        APROVADO, REJEITADO, PENDENTE, ARQUIVADO
+    }
 }
