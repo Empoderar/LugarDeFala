9cc4d8b2d0252ce8f60b2b7ff7f4bef97475e8e3
Maria Fernanda <mariafernandanrengel@gmail.com>
2023-12-18 14:45:30 -0300

    Correção do hibernate e classe Principal

--- src/main/java/br/senac/lugardefala/visao/Principal.java
+++ src/main/java/br/senac/lugardefala/visao/Principal.java
@@ -1,9 +1,5 @@
 package br.senac.lugardefala.visao;
 
-import br.senac.lugardefala.modelo.dao.contato.ContatoDAO;
-import br.senac.lugardefala.modelo.dao.contato.ContatoDAOImpl;
-import br.senac.lugardefala.modelo.dao.relato.RelatoDAO;
-import br.senac.lugardefala.modelo.dao.relato.RelatoDAOImpl;
 import br.senac.lugardefala.modelo.dao.usuario.UsuarioDAO;
 import br.senac.lugardefala.modelo.dao.usuario.UsuarioDAOImpl;
 import br.senac.lugardefala.modelo.entidade.usuario.Usuario;
@@ -13,17 +9,12 @@ public class Principal {
 	public static void main(String[] args) {
 
 		UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
-		ContatoDAO contatoDAO = new ContatoDAOImpl();
-		RelatoDAO relatoDAO = new RelatoDAOImpl();
 
-		String nome = "Allan";
-		String cpf = "123.456.789-10";
-
+		String nome = "Brenda";
 		Usuario usuario = new Usuario();
 
 		usuario.setNome(nome);
 
 		usuarioDAO.inserirUsuario(usuario);
-
 	}
 }
