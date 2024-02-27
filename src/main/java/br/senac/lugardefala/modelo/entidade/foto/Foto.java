b754bee1227e7f102b8e81f3321cb9431343eb4f
Brenda <eisingbrenda@gmail.com>
2024-02-27 19:57:49 -0300

    Add a parte do João

--- src/main/java/br/senac/lugardefala/modelo/entidade/foto/Foto.java
+++ src/main/java/br/senac/lugardefala/modelo/entidade/foto/Foto.java
@@ -1,6 +1,7 @@
 package br.senac.lugardefala.modelo.entidade.foto;
 
 import javax.persistence.Entity;
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -8,6 +9,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Lob;
 
 import br.senac.lugardefala.modelo.entidade.usuario.Usuario;
 
@@ -22,15 +24,27 @@ public class Foto{
     
     @OneToOne(mappedBy = "foto")
     private Usuario usuario;
+    
+    @Lob
+    @Column(name = "binario_foto", nullable = false)
+    private byte[] binario;
 
+    @Column(name = "caminho_foto", nullable = false)
     private String caminho;
 
     public Foto() {
     }
 
 
-    public Foto(String caminho) {
-        this.caminho = caminho;
+    public Foto(String caminho, byte[] binario) {
+    	setCaminho(caminho);
+    	setBinario(binario);
+    }
+
+    public Foto(Long id,String caminho, byte[] binario) {
+    	setId(id);
+    	setCaminho(caminho);
+    	setBinario(binario);
     }
 
 
@@ -56,6 +70,15 @@ public class Foto{
 
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
+    }
+    
+    public byte[] getBinario() {
+    	return binario;
+    }
+
+
+    public void setBinario(byte[] binario) {
+    	this.binario = binario;
     }
 
 }
